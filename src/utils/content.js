d85084eb42cde73d020502a68a260dd198451f56
Phinneas <buzzuw2@gmail.com>
2026-04-24 19:42:05 -0700

    fix: routes and content.js, index.astro, astro.config

--- src/utils/content.js
+++ src/utils/content.js
@@ -2,12 +2,14 @@ import { getCollection } from "astro:content";
 
 // Only return posts without `draft: true` in the frontmatter
 
-export const latestPosts = (
-  await getCollection("blog", ({ data }) => {
+export async function getLocalPosts() {
+  const posts = await getCollection("blog", ({ data }) => {
     return data.draft !== true;
-  })
-).sort(
-  (a, b) =>
-    new Date(b.data.publishDate).valueOf() -
-    new Date(a.data.publishDate).valueOf()
-);
+  });
+  
+  return posts.sort(
+    (a, b) =>
+      new Date(b.data.publishDate).valueOf() -
+      new Date(a.data.publishDate).valueOf()
+  );
+}
