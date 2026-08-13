d7910e1917cb112c73cd291610c1c197d2a50fe6
Phinneas <buzzuw2@gmail.com>
2026-08-13 13:10:07 -0700

    Remove vite loadEnv

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,6 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import cloudflare from '@astrojs/cloudflare';
 import { unified } from '@astrojs/markdown-remark';
-import { loadEnv } from 'vite';
-
-const env = loadEnv(process.env.NODE_ENV || 'production', process.cwd(), '');
 
 import rehypeImageNativeLazyLoading from 'rehype-plugin-image-native-lazy-loading';
 
@@ -28,7 +25,7 @@ export default defineConfig({
   },
   markdown: {
     processor: unified({
-      remarkPlugins: [remarkReadingTime, [remarkNapkin, { apiKey: env.NAPKIN_API_KEY || process.env.NAPKIN_API_KEY || env.NAPKIN_KEY }]],
+      remarkPlugins: [remarkReadingTime, [remarkNapkin, { apiKey: process.env.NAPKIN_API_KEY || process.env.NAPKIN_KEY }]],
       rehypePlugins: [rehypeImageNativeLazyLoading],
     }),
   },
